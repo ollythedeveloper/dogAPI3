6fe7d4466689ab27726e3ceb714077769b9f5846
Olivia Lewis <ollybaby12@gmail.com>
2020-12-29 17:15:49 -0500

    added lowerCase catch

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 'use strict';
 
-function getDogImage(dogBreed) {
-  fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
+//This function places the input breed into the GET(fetch) Request
+//then a JSON response is generated
+//then the response is put through the displayImage function
+//if any errors are caught an alert will be displayed
+function getDogImage(lowerDog) {
+  fetch(`https://dog.ceo/api/breed/${lowerDog}/images/random`)
     .then(response => response.json())
     .then(responseJson => displayImage(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+//This function takes the responseJSON, creates images,
+//and places them in the DOM
+//If there is a code '404' error then a response message will be displayed
 function displayImage(responseJson){
  if (responseJson.code == '404') {
  $('.imgResult').replaceWith(`<h3 class="imgResult">${responseJson.message}.</h3>`)
@@ -19,12 +26,14 @@ function displayImage(responseJson){
  }
 }
 
+//This function responds to the form submission by taking the input breed
+//and placing it into the GET Request
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     let dogBreed = $('#dogType').val();
-    console.log(dogBreed);
-    getDogImage(dogBreed);
+    let lowerDog = dogBreed.toLowerCase();
+    getDogImage(lowerDog);
     $('#dogType').val('');
   })
 }
